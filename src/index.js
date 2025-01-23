9717d4dd7b787dddfafbbeff9f444bbddc434754
Divine <divinematibhiri2003@gmail.com>
2025-01-23 15:10:45 +0200

    first commit

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./styles/global.css"; // Optional global styles
-
-ReactDOM.render(<App />, document.getElementById("root"));
